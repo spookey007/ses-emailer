e305a4bfb25c2586ced8274fe35168ee2fba95cb
Fahad Bashir <41535399+spookey007@users.noreply.github.com>
2024-07-02 23:23:25 +0500

    Create error.php

---
+++ error.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Error</title>
+</head>
+<body>
+    <h1>Access Denied</h1>
+    <p>Incorrect password!</p>
+    <a href="index.php">Try again</a>
+</body>
+</html>
